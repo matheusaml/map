0352aee014ce58871ea04920c7078262576359b5
matheusaml <mmelucena@gmail.com>
2018-09-18 18:15:04 -0300

    PNG

---
+++ Atividade8/src/ImagemPNG.java
@@ -0,0 +1,13 @@
+public class ImagemPNG implements Imagem {
+
+	@Override
+	public void carregarImagem() {
+		System.out.println("Carregando imagem png");
+	}
+
+	@Override
+	public void desenharImagem() {
+		System.out.println("Desenhando imagem png");
+	}
+
+}
